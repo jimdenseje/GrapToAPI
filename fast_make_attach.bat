5d21e3e9cd87d85ca2d8fc381e3564af00c44b00
Jim Haaland Damgaard <jimdenseje@gmail.com>
2022-08-27 00:24:14 +0200

    version 1.0

--- fast_make_attach.bat
+++ fast_make_attach.bat
@@ -1,8 +1,10 @@
 docker stop graptoapi
 docker rm graptoapi
-docker rmi graptoapi
-docker build -t graptoapi .
+docker rmi jimdenseje/graptoapi
+docker build -t jimdenseje/graptoapi .
 
-docker run -d -p 8000:80 -v graptoapi-mysql:/var/lib/mysql:rw -v graptoapi-sides:/var/www/selenium/sides:rw --name graptoapi graptoapi
+docker run -d -p 8000:80 -v graptoapi-mysql:/var/lib/mysql:rw -v graptoapi-sides:/var/www/html/selenium/sides:rw --name graptoapi jimdenseje/graptoapi
 
 docker exec -it graptoapi /bin/bash
+
+pause
