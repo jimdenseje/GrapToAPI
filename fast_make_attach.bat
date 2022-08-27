5ce8c7c2fa4225a453ded587ad64efc8dd5a858d
Jim Haaland Damgaard <jimdenseje@gmail.com>
2022-08-27 17:12:42 +0200

    v1.2

--- fast_make_attach.bat
+++ fast_make_attach.bat
@@ -1,9 +1,29 @@
+@echo off
+
+echo ######### Last Worked On Version #########################
+docker images ls -a --filter reference=jimdenseje/graptoapi*
+
+echo.
+
+echo ######### Last Realise Version On HUB.DOCKER.COM ##########
+FOR /F "tokens=*" %%g IN ('C:\php\php.exe "current_relised_version.php"') do (SET current=%%g)
+echo %current%
+
+echo.
+
+set /p version="Add Version tag for this image (>%current%): " 
+
 docker stop graptoapi
 docker rm graptoapi
-docker rmi jimdenseje/graptoapi
-docker build -t jimdenseje/graptoapi .
 
-docker run -d -p 8000:80 -v graptoapi-mysql:/var/lib/mysql:rw -v graptoapi-sides:/var/www/html/selenium/sides:rw --name graptoapi jimdenseje/graptoapi
+rem list all images starting with ?
+FOR /F "tokens=*" %%g IN ('docker images ls -aq --filter reference^=jimdenseje/graptoapi*') do (SET VAR=%%g)
+docker rmi --force %VAR%
+
+docker build -t jimdenseje/graptoapi:%version% .
+docker image tag jimdenseje/graptoapi:%version% jimdenseje/graptoapi
+
+docker run -d -p 8000:80 -v graptoapi-mysql:/var/lib/mysql:rw -v graptoapi-sides:/var/www/html/selenium/sides:rw --name graptoapi jimdenseje/graptoapi:%version%
 
 docker exec -it graptoapi /bin/bash
 
--- www/selenium/run_sides.php
+++ www/selenium/run_sides.php
@@ -2,6 +2,15 @@
 
 include("/var/www/html/functions/mysql_db.php");
 
+function get_string_between($start, $end, $string){
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+}
+
 while (true) {
 
 	$jobs = mysql_DB_SQL_array(
@@ -26,6 +35,17 @@ while (true) {
 				}
 				$count++;
 			}
+			
+			//add last run to json out
+			//$parse_end_values = "Test Suites:".@explode("Test Suites:", $data)[1];
+			$values["GrapToApiProp"]["LastRun"] = date("Y-m-d H:i:s");
+			$values["GrapToApiProp"]["PassedTest"] = trim(get_string_between("Test Suites: ", "passed", $data));
+			$values["GrapToApiProp"]["ExecutionTime"] = trim(get_string_between("Time:", ",", $data));
+			if ($values["GrapToApiProp"]["ExecutionTime"] == "") {
+				$values["GrapToApiProp"]["ExecutionTime"] = trim(get_string_between("Time:", "\n", $data));
+			}
+			$values["GrapToApiProp"]["ExecutionTimeEstimated"] = trim(get_string_between("estimated", "\n", $data));
+			
 			file_put_contents("data/".$filename.".json", json_encode($values, JSON_PRETTY_PRINT));
 
 			file_put_contents("last_run/".$filename.".txt", $data);
